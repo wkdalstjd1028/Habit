a0c7479b4e4e4e684119eaf0a89b971a1d19e0ba
ndri28 <89716903+ndri28@users.noreply.github.com>
2025-11-22 19:37:38 +0900

    Update HabitController.java
    1. 메인화면 간단 연동

--- src/main/java/com/project/habit/habit/controller/HabitController.java
+++ src/main/java/com/project/habit/habit/controller/HabitController.java
@@ -1,4 +1,11 @@
 package com.project.habit.habit.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HabitController {
+  
+  @GetMapping("/")
+    public String home(){
+        return "1";
+    }
 }
